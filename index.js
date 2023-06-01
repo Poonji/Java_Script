c75e34bd37e8b43fc4bf0eed6417fa63fcacf1ea
Poonji <poonjij21@gmail.com>
2023-06-01 23:38:12 +0530

    arrays

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 function clicked(){
     console.log('the button was clicked')
 }
+window.onload=function(){
+   
+    console.log('the document was loaded')
+}
+
+firstContainer.addEventListner('click',function(){
+    console.log("click hua")
+})
