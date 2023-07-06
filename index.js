7259629698605b545dfb27cc582bd7271a5cc791
Poonji <poonjij21@gmail.com>
2023-07-06 23:19:08 +0530

    done with objects and functions

--- index.js
+++ index.js
@@ -10,3 +10,22 @@ window.onload=function(){
 firstContainer.addEventListner('click',function(){
     console.log("click hua")
 })
+
+console.log('hello world!');
+//objects in js
+let rectangle={
+    length:1,
+    breath:2
+};
+    // factory function
+    function createRectangle(length, breadth)
+    {
+        return rectangle={
+            length:length,
+            breath:breadth
+        };
+    }
+let rectangle1=createRectangle(3,4);
+let rectangle3=createRectangle(4,4);
+let rectangle2=createRectangle(2,4);
+
