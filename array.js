b732acf2ef316abd08e76187b815b36d7320ab3e
Poonji <poonjij21@gmail.com>
2023-07-19 16:14:17 +0530

    call back function

--- array.js
+++ array.js
@@ -1,2 +1,16 @@
-let numbers=[1,6,3,2,4,,56]
+/* let numbers=[1,6,3,2,4,,56]
 console.log(numbers);
+console.log(numbers.includes(4));
+console.log(numbers.includes(10));
+console.log(numbers.indexOf(3,1)); */
+
+let courses=[
+                {no:1,naam:'Love'},
+                {no:2,naam:'Rahul'}
+];
+console.log(courses);
+let x= courses.find(function(x)
+{
+    return x.naam=='Love';
+})
+console.log(x);
